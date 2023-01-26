e4f8b846af9da36cebcc4d89b42727094d65d626
Lalawmpuia Chawngthu <awmach.9@gmail.com>
2023-01-26 13:06:59 +0530

    [update]: - vite config: remove views folder from input - run UserSeeder

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
-                "resources/views/**",
-            ],
+            input: ["resources/js/app.js"],
             refresh: true,
         }),
     ],
