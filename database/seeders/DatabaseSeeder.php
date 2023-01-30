69d8a11f8dcc2e6fad01fc01a550986cece276e2
Lalawmpuia Chawngthu <awmach.9@gmail.com>
2023-01-30 16:06:55 +0530

    update: - unauthenticated reroute to home - added BaseModel - navigation/item component can accept addl. classes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ProductSeeder::class,
+            // Users' products can be seeded with UserProductSeeder
         ]);
     }
 }
--- resources/views/components/navigation/item.blade.php
+++ resources/views/components/navigation/item.blade.php
@@ -1,3 +1,9 @@
+@php
+    $attributes['class'] = 'focus:outline-sky-200 focus:outline-1' . " {$attributes['class']}";
+@endphp
 <li class="p-2 hover:bg-sky-800 hover:text-white hover:cursor-pointer">
-    <a href="{{ $link }}">{{ $label }}</a>
+    <a
+        {{ $attributes }}
+        href="{{ $link }}"
+    >{{ $label }}</a>
 </li>
