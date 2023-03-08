4b6fa02a24ba49c2ee110bdc6239eb8fe66c36fa
Lalawmpuia Chawngthu <awmach.9@gmail.com>
2023-03-08 12:56:17 +0530

    refactor: nav link

--- resources/views/components/navigation/item.blade.php
+++ resources/views/components/navigation/item.blade.php
@@ -1,9 +1,11 @@
-@php
+{{-- @php
     $attributes['class'] = 'focus:outline-sky-200 focus:outline-1' . " {$attributes['class']}";
-@endphp
-<li class="p-2 hover:bg-sky-800 hover:text-white hover:cursor-pointer">
-    <a
-        {{ $attributes }}
-        href="{{ $link }}"
-    >{{ $label }}</a>
-</li>
+@endphp --}}
+<a
+    {{ $attributes->merge(['class' => 'focus:outline-sky-200 focus:outline-1']) }}
+    href="{{ $link }}"
+>
+    <li class="p-2 hover:bg-sky-800 hover:text-white hover:cursor-pointer">
+        {{ $label }}
+    </li>
+</a>
